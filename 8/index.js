9c1224cbe4aa39bc28c58af9537a075449154371
doodoodoong <doo_doong@hanmail.net>
2023-01-31 02:03:21 +1000

    complete

--- 8/index.js
+++ 8/index.js
@@ -1,32 +1,54 @@
-const generateNumber = document.querySelector("#generateNumber");
-const guessNumber = document.querySelector("#guessNumber");
-const btnPlay = document.querySelector("body > div.guessNumber > button");
+const generateInput = document.querySelector("#generate-number");
+const guessInput = document.querySelector("#guess-number");
+const btnPlay = document.querySelector("#guess-number > button");
+const youChose = document.querySelector("body > div > span.you-chose");
+const machineChose = document.querySelector("body > div > span.machine-chose");
+const displayResult = document.querySelector("body > div > p.display-result");
 
-function gNumber(e) {
-  const number = e.target.value;
-  console.log(number);
-  const randomNumber = Math.floor(Math.random() * parseInt(number));
-  console.log(randomNumber);
+function generateNumber(e) {
+  e.preventDefault();
+  const generateNumber = document.querySelector(
+    "#generate-number > input[type=number]"
+  ).value;
+
+  const randomNumber = Math.ceil(Math.random() * parseInt(generateNumber));
   localStorage.setItem("randomNumber", randomNumber);
 }
 
-function chooseNumber(e) {
-  const number = e.target.value;
-  console.log(number);
-  console.log(typeof number);
-  localStorage.setItem("chooseNumber", number);
+function guessNumber(e) {
+  e.preventDefault();
+  const guessNumber = document.querySelector(
+    "#guess-number > input[type=number]"
+  ).value;
+  localStorage.setItem("guessNumber", guessNumber);
 }
 
 function judge() {
   const randomNumber = parseInt(localStorage.getItem("randomNumber"));
-  const chooseNumber = parseInt(localStorage.getItem("chooseNumber"));
-  if (randomNumber === chooseNumber) {
-    console.log("correct");
-  } else {
-    console.log("fail");
+  const guessNumber = parseInt(localStorage.getItem("guessNumber"));
+  if (
+    localStorage.getItem("randomNumber") !== null &&
+    localStorage.getItem("guessNumber") !== null
+  ) {
+    youChose.innerText = `You chose: ${guessNumber}, `;
+    machineChose.innerText = `the machine chose: ${randomNumber}`;
+    if (randomNumber === guessNumber) {
+      displayResult.innerText = "You Win!";
+    } else {
+      displayResult.innerText = "You lost!";
+    }
   }
+
+  generateNewNumber(
+    document.querySelector("#generate-number > input[type=number]").value
+  );
 }
 
-generateNumber.addEventListener("change", gNumber);
-guessNumber.addEventListener("change", chooseNumber);
+function generateNewNumber(e) {
+  const newRandomNumber = Math.ceil(Math.random() * e);
+  localStorage.setItem("randomNumber", newRandomNumber);
+}
+
+generateInput.addEventListener("submit", generateNumber);
+guessInput.addEventListener("submit", guessNumber);
 btnPlay.addEventListener("click", judge);
